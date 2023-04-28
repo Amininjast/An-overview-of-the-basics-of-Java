98f4c0ebe3938caff040049d3dd05b3afe07a6c7
Amininjast <aminarad228@gmail.com>
2023-04-28 19:03:02 +0330

    //old version of switch

--- src/com/amininjast/Main.java
+++ src/com/amininjast/Main.java
@@ -4,5 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Amin Injast");
+        char a = 'A';
+        //old version of switch
+        switch (a){
+            case 'A':
+                System.out.println("Its A");
+                break;
+            case 'B':
+                System.out.println("Its B");
+                break;
+            case 'C':
+            case 'D':
+                System.out.println("Its C or D");
+                break;
+        }
     }
 }
