2a4e956850930ef783c51e684115d65529dfe4d3
Amininjast <aminarad228@gmail.com>
2023-05-19 00:37:02 +0330

    write to thr File

--- src/com/amininjast/WorkWithFile.java
+++ src/com/amininjast/WorkWithFile.java
@@ -1,21 +1,42 @@
 package com.amininjast;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class WorkWithFile {
     public static void main(String[] args) {
+        File file = createFile("src/Amin.txt");
+        writeFile(file, true);
+
+    }
+
+    private static void writeFile(File file, boolean append) {
         try {
-            File file = new File("src/amin.txt");
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            if (file.exists()) {
-                file.delete();
-            }
+            FileWriter fileWriter = new FileWriter(file, append);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println("Amin");
+            printWriter.println("");
+            printWriter.println("INJAST");
+            printWriter.flush();
+            printWriter.close();
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
 
+    private static File createFile(String path) {
+        try {
+            File file = new File(path);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            return file;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new IllegalStateException(e);
+        }
     }
 }
