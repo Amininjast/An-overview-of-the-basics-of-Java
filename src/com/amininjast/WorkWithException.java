a9eb0a4b468c78d2f300b6a346058900d0841793
Amininjast <aminarad228@gmail.com>
2023-05-18 17:12:34 +0330

    more about Exception

--- src/com/amininjast/WorkWithException.java
+++ src/com/amininjast/WorkWithException.java
@@ -3,10 +3,15 @@ package com.amininjast;
 public class WorkWithException {
     public static void main(String[] args) {
         try {
-            int number = Integer.parseInt("1x");
+            int number = Integer.parseInt("1");
             System.out.println(number);
-        } catch (NumberFormatException e) {
-            System.out.println("Your String isnt a number :)");
+            int number2 = 10 / 0;
+            System.out.println(number2);
+        } catch (NumberFormatException | ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("All Exception");
+            System.out.println(e.getMessage());
         }
 
     }
