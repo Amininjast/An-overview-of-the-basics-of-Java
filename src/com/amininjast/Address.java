11e4a40d6104f7f36e29cf64bbf33ec667ad8776
Amininjast <aminarad228@gmail.com>
2023-05-21 20:57:26 +0330

    Composition

---
+++ src/com/amininjast/Address.java
@@ -0,0 +1,9 @@
+package com.amininjast;
+
+public class Address {
+    private String country;
+    private String city;
+    private int zone;
+    private String Street;
+    private int no;
+}
---
+++ src/com/amininjast/Person.java
@@ -0,0 +1,9 @@
+package com.amininjast;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Address address;
+    private String country;
+
+}
