c70ab45e849145a9009e9183bd87e0f7201dd1f2
Amininjast <aminarad228@gmail.com>
2023-07-07 20:02:41 +0330

    date structures - HashMap - hashcode

--- src/com/amininjast/datastructures/TheMap.java
+++ src/com/amininjast/datastructures/TheMap.java
@@ -2,9 +2,19 @@ package com.amininjast.datastructures;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class TheMap {
     public static void main(String[] args) {
+        Map<Person, Dimond> map = new HashMap<>();
+        map.put(new Person("Amin"), new Dimond("Iraq"));
+        map.put(new Person("Amin"), new Dimond("Iran"));
+        System.out.println(map.size());
+        System.out.println(map.entrySet());
+
+    }
+
+    private static void maps() {
         Map<Integer, Person> map = new HashMap<>();
         map.put(1, new Person("Amin"));
         map.put(2, new Person("Ali"));
@@ -21,7 +31,35 @@ public class TheMap {
         System.out.println(map.entrySet());
     }
 
-    record Person(String name) {
+    static class Person {
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    record Dimond(String name) {
     }
 }
 
