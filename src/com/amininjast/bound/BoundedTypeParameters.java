ebd1de4aa5da025869e8aea717d96d1278993489
Amin Haddad <mhadad@payeshgaran.net>
2023-07-22 14:04:40 +0330

    Bounded Type Parameters

--- src/com/amininjast/bound/BoundedTypeParameters.java
+++ src/com/amininjast/bound/BoundedTypeParameters.java
@@ -2,15 +2,18 @@ package com.amininjast.bound;
 
 public class BoundedTypeParameters {
     public static void main(String[] args) {
-        int number = 2;
-        int[] numbers = {1, 2, 3, 4, 5};
+        Integer number = 2;
+        Integer[] numbers = {1, 2, 3, 4, 5};
         System.out.println(countGreaterThan(numbers, number));
+        Double number2 = 3.1;
+        Double[] numbers2 = {1.2, 2.2, 3.1, 3.2, 3.3, 4.2, 5.3};
+        System.out.println(countGreaterThan(numbers2, number2));
     }
 
-    public static int countGreaterThan(int[] numbers, int number) {
+    static <T extends Comparable> int countGreaterThan(T[] numbers, T number) {
         int count = 0;
-        for (int a : numbers) {
-            if (a > number) {
+        for (T a : numbers) {
+            if (a.compareTo(number) > 0) {
                 count++;
             }
         }
